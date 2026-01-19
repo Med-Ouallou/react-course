014f7dc610889f961b922e82702277c350bd27f1
Med-Ouallou <mouallou279@gmail.com>
2026-01-19 16:58:37 +0000

    update ToggleChallenge component to include state management and alert functionality

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isTogel,setTogel] = useState(false);
+
+  const handelClick = ()=> {
+      
+  }
+  
+
+  return (
+    <div>
+        <button className="btn">Togel Alert</button>
+        <Alert/>
+    </div>
+  )
 };
+
+const Alert = ()=>{
+  return(
+    <div className="alert alert-danger">Hello World</div>
+  )
+}
 
 export default ToggleChallenge;
